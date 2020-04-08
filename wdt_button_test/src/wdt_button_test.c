7d22710692fa351354c171db93c1a354fc7cc35b
Darren Huang <Darren.Huang@advantech.com.tw>
2020-04-08 09:35:26 +0800

    add sw wdt button test tool

---
+++ wdt_button_test/src/wdt_button_test.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <linux/watchdog.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <string.h>
+#include <linux/input.h>
+
+#include "test_utils.h"
+
+#define TRUE		1
+#define FALSE		0
+#define	GPIO_NAME	"gpio-keys"
+
+void help_info(void);
+
+int getEventName(char* ret_string)
+{
+	int i = 0;
+	char event_name[64], event_path[28], cmd[64] , buf[16];
+	FILE *fp;
+	
+	while (1) {
+		sprintf(cmd, "cat /sys/class/input/input%d/name", i);
+		fp = popen(cmd , "r");
+		fgets(event_name, sizeof(event_name), fp);
+		//printf("event%d name = %s\n",i,event_name);
+		if(strstr(event_name , GPIO_NAME ) != NULL) {
+			sprintf(event_path, "/dev/input/event%d", i);
+			event_path[strlen(event_path)] = '\0';
+			strncpy(ret_string, event_path, strlen(event_path));
+			pclose(fp);
+			return TRUE;
+		} else {
+			i++;
+			memset(buf ,'\0', sizeof(buf));	
+			sprintf(cmd, "test -d /sys/class/input/input%d && echo Exist",i);
+			fp = popen(cmd , "r");
+			fgets(buf, sizeof(buf), fp);
+			if ( strstr(buf , "Exist" ) == NULL ) {
+				pclose(fp);
+				return FALSE;
+			}
+			
+		}
+		
+	}
+}
+
+int main(int argc, char * const argv[])
+{
+	int fd, wdt_fd, timeout, test, ret;
+	char event_cmd[28];
+	struct input_event inputevent;
+
+	print_name(argv);
+
+	if (argc < 2) {
+		help_info();
+		return 1;
+	}
+	timeout = atoi(argv[1]);
+	test = atoi(argv[2]);
+	printf("Starting wdt_driver (timeout: %d, test: %s)\n",
+	       timeout, (test == 0) ? "ioctl" : "write");
+
+	// Get Event ID
+	memset(event_cmd,'\0', sizeof(event_cmd));	
+
+	if(!getEventName(event_cmd)) {
+		printf("%s: Can't get %s event\n", argv[0], GPIO_NAME);
+		exit(1);
+	}
+
+	fd = open(event_cmd, O_RDONLY);
+	if (fd < 0) {
+		printf("%s: Open gpio-keys failed.\n", argv[0]);
+		exit(1);
+	} else
+		printf("%s: Open gpio-keys success.\n", argv[0]);
+	
+	wdt_fd = open("/dev/watchdog", O_WRONLY);
+	if (wdt_fd == -1) {
+		perror("watchdog");
+		exit(1);
+	}
+
+	printf("Trying to set timeout value=%d seconds\n", timeout);
+	ioctl(wdt_fd, WDIOC_SETTIMEOUT, &timeout);
+	printf("The actual timeout was set to %d seconds\n", timeout);
+	ioctl(wdt_fd, WDIOC_GETTIMEOUT, &timeout);
+	printf("Now reading back -- The timeout is %d seconds\n", timeout);
+	
+	while (1) {
+		ret = read(fd, &inputevent, sizeof(struct input_event));
+		if(ret <= 0) {
+			printf("read fail!\n");
+			exit(1);
+		}
+
+		switch(inputevent.type){
+			case EV_KEY:
+				switch(inputevent.code) {
+					case KEY_PROG1: /* SW WDT triger*/
+						if(!inputevent.value) {
+							if (test == 0) {
+								ioctl(wdt_fd, WDIOC_KEEPALIVE, 0);
+							} else {
+								write(wdt_fd, "\0", 1);
+							}
+						}
+					break;
+				}
+			break;
+		}
+	}
+
+	print_result(argv);
+
+	close(wdt_fd);
+	close(fd);
+	return 0;
+}
+
+void help_info(void)
+{
+	printf("Usage: wdt_driver_test <timeout> <test>\n");
+	printf("    timeout: value in seconds to cause wdt timeout/reset\n");
+	printf("    test: 0 - Service wdt with ioctl(), 1 - with write()\n");
+}
