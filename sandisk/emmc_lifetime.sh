d245918434c44a0981363bac44d92ae1c0ba5c64
yanwei.cao <yanwei.cao@advantech.com.cn>
2023-07-06 17:01:31 +0800

    fix log file path

--- sandisk/emmc_lifetime.sh
+++ sandisk/emmc_lifetime.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-ls /dev/mmc* > emmc_life.log 2>&1
-GETPATH=`grep "boot" emmc_life.log |awk 'NR==1{print $1}'`
+ls /dev/mmc* > /mnt/data/emmc_life.log 2>&1
+GETPATH=`grep "boot" /mnt/data/emmc_life.log |awk 'NR==1{print $1}'`
 DEVICE=`echo ${GETPATH:0:12}`
 
-ecsd $DEVICE 187 4 > emmc_life.log 2>&1
-ecsd $DEVICE 39 4 >> emmc_life.log 2>&1
-DEVICE_HEALTH_USED=`cat emmc_life.log|awk 'NR==2{print $5}'`
-DEVICE_BAD_BLK=`cat emmc_life.log|awk 'NR==4{print $5}'`
-OUTPUT=`grep "[-]" emmc_life.log`
+ecsd $DEVICE 187 4 > /mnt/data/emmc_life.log 2>&1
+ecsd $DEVICE 39 4 >> /mnt/data/emmc_life.log 2>&1
+DEVICE_HEALTH_USED=`cat /mnt/data/emmc_life.log|awk 'NR==2{print $5}'`
+DEVICE_BAD_BLK=`cat /mnt/data/emmc_life.log|awk 'NR==4{print $5}'`
+OUTPUT=`grep "[-]" /mnt/data/emmc_life.log`
 
-rm emmc_life.log
+rm /mnt/data/emmc_life.log
 sync
 
 if [[ x$OUTPUT != x ]];then
-	echo "Life loss:$((0x$DEVICE_HEALTH_USED))%"
-	echo "Bad block:$((0x$DEVICE_BAD_BLK))"
+	echo "Percentage of eMMC used:$((0x$DEVICE_HEALTH_USED))%"
+	echo "Bad block num:$((0x$DEVICE_BAD_BLK))"
 else
 	echo "Read error!"
 fi
